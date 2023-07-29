7878ce19e6570f99da84934e36658e165aca4631
Lasse Meixner <lassemeixner@gmail.com>
2023-07-29 13:22:35 +0200

    filter zips

--- US/lead_data/reuters/scripts/source files/clean-DC.R
+++ US/lead_data/reuters/scripts/source files/clean-DC.R
@@ -36,7 +36,7 @@ dc <-  bind_rows(`2005`,`2006`,`2007`,`2008`,`2009`,`2010`,`2011`,`2012`,`2013`,
 dc <- dc %>% 
   rename(zip = `Zip.Code`) %>% 
   mutate(n=nchar(zip)) %>% 
-  mutate(zip=ifelse(n==2,paste0("000",zip),
+  mutate(zip=ifelse(n==2,paste0("000",zip), #NOTE: seems to me there are no such zips?? There are only 5 and 7.
                     ifelse(n==3,paste0("00",zip),
                            ifelse(n==4,paste0("0",zip),zip)))) %>% 
   mutate(tested=replace(tested, tested == "(b)(6)", "<5"))%>%
@@ -44,7 +44,9 @@ dc <- dc %>%
   mutate(BLL_geq_10=replace(BLL_geq_10, BLL_geq_10 == "(b)(6)", "<5"))%>%
   mutate(state = 'DC') %>%
   relocate(state) %>% 
-  mutate(year=factor(year))
+  mutate(year=factor(year)) %>%
+  filter(n!=7) # Filter out "Missing" zip
+
   
   
 # remove unnecessary variables
