0a93a3ab8546d0b80fa50db854bee5e2bae298dd
Lasse Meixner <lassemeixner@gmail.com>
2023-07-29 20:43:06 +0200

    fix

--- US/lead_data/reuters/scripts/source files/clean-AZ.R
+++ US/lead_data/reuters/scripts/source files/clean-AZ.R
@@ -25,11 +25,11 @@ az <- read_excel(az_path, sheet ='ALL',skip=2) %>%
   rename(BLL_leq_5 = `<5 µg/dL`, 
          BLL_geq_10 = `10+ µg/dL`,
          BLL_59=as.integer(`5-9 µg/dL`)) %>% 
-  mutate(tested=BLL_leq_5+BLL_geq_10+BLL_59) %>% 
-  mutate(BLL_geq_5 = BLL_59+BLL_geq_10) %>% 
-  rename(zip=`ZIP CODE`,
-         county = COUNTY,
+  mutate(tested = BLL_leq_5+BLL_geq_10+BLL_59,
+         BLL_geq_5 = BLL_59+BLL_geq_10,
          year = factor(year),
-         state = "AZ")
+         state = "AZ") %>% 
+  rename(zip=`ZIP CODE`,
+         county = COUNTY)
 # save to csv
 write_csv(az, "../processed_files/az.csv")
