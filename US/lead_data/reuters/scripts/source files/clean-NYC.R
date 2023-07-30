bac00daca826994321eab577d4a1c55d15f8c88a
Lasse Meixner <lassemeixner@gmail.com>
2023-07-30 15:20:26 +0200

    borough fix & readability

--- US/lead_data/reuters/scripts/source files/clean-NYC.R
+++ US/lead_data/reuters/scripts/source files/clean-NYC.R
@@ -44,16 +44,16 @@ nyc <- nyc %>%
   mutate(state = 'NYC') %>%
   relocate(state) %>%
   mutate(year = factor(year)) %>% 
-  filter(str_detect(tract,"^0061")|str_detect(tract,"^0047")|str_detect(tract,"^0081")|str_detect(tract,"^0085")|str_detect(tract,"^0029")) %>% 
+  filter(str_detect(tract,"^0061")|str_detect(tract,"^0047")|str_detect(tract,"^0081")|str_detect(tract,"^0085")|str_detect(tract,"^0005")) %>%  # filter for the 5 boroughs/counties
   mutate(tract=substring(tract,2)) %>% 
   mutate(tract=paste0("36",tract)) %>% 
   mutate(n=nchar(tract)) %>% 
-  mutate(tract=ifelse(n==9,paste0(tract,"00"),
-                      ifelse(n==10,paste0(paste0(substring(tract,1,5),"0"),substring(tract,6,10)),tract)))  %>% 
+  mutate(tract=ifelse(n==9,paste0(tract,"00"), # I believe this introduces some errors into the tracts!
+                      ifelse(n==10,paste0(substring(tract,1,5),"0",substring(tract,6,10)),tract)))  %>% 
   distinct()
   
 # remove unnecessary variables
-rm(nycraw, `2005`,`2006`,`2007`,`2008`,`2009`,`2010`,`2011`,`2012`,`2013`,`2014`,`2015`)
+rm(nycraw, df, `2005`,`2006`,`2007`,`2008`,`2009`,`2010`,`2011`,`2012`,`2013`,`2014`,`2015`)
 
 # save to csv
 write_csv(nyc, file = "../processed_files/nyc.csv")
