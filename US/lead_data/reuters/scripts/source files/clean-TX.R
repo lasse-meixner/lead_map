4fded4a353ba80f6e2f671c36020244e999d3174
Lasse Meixner <lassemeixner@gmail.com>
2023-07-30 17:31:56 +0200

    sum-safe suppression

--- US/lead_data/reuters/scripts/source files/clean-TX.R
+++ US/lead_data/reuters/scripts/source files/clean-TX.R
@@ -20,12 +20,26 @@ tx <- read_sas(tx_path) %>%
          zip = ZIP,
          tested = NUM_TESTED,
          BLL_geq_10 = BLL_GE_10) %>% 
-  mutate(tested=replace_na(as.numeric(tested),0),
-         BLL_geq_10=replace_na(as.numeric(BLL_geq_10),0),
-         BLL_5_9=replace_na(as.numeric(BLL_5_9),0)) %>% 
-  mutate(BLL_geq_5 = BLL_geq_10 + BLL_5_9) %>% 
-  mutate(year=factor(year))
-         
+  # get rid of spaces in suppressed values
+  mutate(BLL_5_9 = replace(BLL_5_9, BLL_5_9 == "< 5", "<5"),
+         BLL_geq_10 = replace(BLL_geq_10, BLL_geq_10 == "< 5", "<5")) %>%
+  # auxiliary cols indicating suppression
+  mutate(BLL_5_9_sup = (BLL_5_9 == "<5"),
+         BLL_geq_10_sup = (BLL_geq_10 == "<5")) %>%
+  # if both are suppressed, then BLL_geq_5 is ">1&<10"
+  # if only one is supprsed, then BLL_geq_5 is the other + ">0&<5"
+  # if none are suppressed, then BLL_geq_5 is the sum of the two
+  mutate(BLL_geq_5 = case_when(
+    BLL_5_9_sup & BLL_geq_10_sup ~ ">1&<10",
+    BLL_5_9_sup ~ paste0(">", as.numeric(BLL_geq_10), "&<", as.numeric(BLL_geq_10) + 5),
+    BLL_geq_10_sup ~ paste0(">", as.numeric(BLL_5_9), "&<", as.numeric(BLL_5_9) + 5),
+    .default = as.character(as.numeric(BLL_5_9) + as.numeric(BLL_geq_10))
+      ),
+    year = factor(year),
+    state = "TX") %>%
+  select(-BLL_5_9_sup, -BLL_geq_10_sup)
+
+
 
 # save to csv
 write_csv(tx, "../processed_files/tx.csv")
