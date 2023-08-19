f13e953250d7d9cc48cafb6970066363e0ea793f
Lasse Meixner <lassemeixner@gmail.com>
2023-08-20 00:52:23 +0200

    Roads computation

--- US/predictors/scripts/04_us_roads.R
+++ US/predictors/scripts/04_us_roads.R
@@ -1,7 +1,6 @@
 ## read in shapefile
-library(googledrive)
-library(tidyverse)
-library(sf)
+required_packages  <- c("googledrive", "tidyverse", "tigris", "sf")
+lapply(required_packages, require, character.only = TRUE)
 
 # check if tl_2021_us_primaryroads.shp exists in "../raw_data/roads", otherwise download all required files from google drive folder
 
@@ -18,3 +17,30 @@ if (!file.exists("../raw_data/roads/tl_2021_us_primaryroads.shp")) {
                          overwrite = TRUE))
 }
 
+# get all us tract geographies for matching
+us_tracts  <- tigris::tracts(cb = TRUE) # get tracts for all states in US
+
+# read in shapefile
+roads <- st_read("../raw_data/roads/tl_2021_us_primaryroads.shp") |>
+  st_simplify(dTolerance = 100) |> # simplify geometry
+  st_join(us_tracts, join = st_intersects) # match road LINESTRING sections to tracts
+
+# get length by type in each tract
+roads_clean  <- roads |> 
+  sample_n(1000) |> # sample 1000 rows (otherwise too slow
+  rename(TRACT = GEOID,
+         TYPE = RTTYP) |> # rename GEOID to TRACT
+  group_by(TRACT, TYPE) |>
+  summarize(LENGTH = sum(st_length(geometry)),
+            ALAND = first(ALAND),
+            .groups = "keep") |>
+  # drop unit and round to nearest integer
+  mutate(LENGTH = as.integer(LENGTH)) |>
+  ungroup() |>
+  pivot_wider(names_from = "TYPE", values_from = "LENGTH", values_fill = 0, id_cols = c("TRACT", "ALAND"), names_prefix = "TYPE_") |> # road types to columns
+  mutate(across(starts_with("TYPE"), ~ . / ALAND, .names = "{.col}_density")) # create density
+
+
+# write to .csv in processed_data
+write_csv(roads_clean, "../processed_data/roads_clean.csv")
+# TODO: Write to googledrive!
