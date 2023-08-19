06c00ed18ee6a97aaf0947c90b78d8374a3c05c6
Lasse Meixner <lassemeixner@gmail.com>
2023-08-20 00:54:13 +0200

    get rid of all ZCTA specific code

--- US/predictors/scripts/04_us_merging.R
+++
@@ -1,5 +0,0 @@
-# US merging 
-
-# Merge SVI data (the only US non-census, non-spatial data) with census data sf object
-
-data_zcta <- left_join(acs_dec_zcta, svi_zcta, by = c("GEOID" = "ZCTA5"))
--- US/predictors/scripts/05_us_exporting.R
+++
@@ -1,9 +0,0 @@
-# US exporting 
-
-# Write US data to a CSV
-
-data_zcta %>% 
-  as.data.frame() %>%
-  select(-geometry) %>%
-  write_csv("data_processed/predictors_zcta_draft.csv")
-
