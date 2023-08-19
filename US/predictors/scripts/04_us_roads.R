fdef933de8e8e77c01d1b4beb68e3600da498d1d
Lasse Meixner <lassemeixner@gmail.com>
2023-08-19 21:57:26 +0200

    load road shapefiles from drive

--- US/predictors/scripts/04_us_roads.R
+++ US/predictors/scripts/04_us_roads.R
@@ -1,7 +1,20 @@
-## read in roads 
+## read in shapefile
+library(googledrive)
+library(tidyverse)
+library(sf)
 
-temp <- tempfile()
-download.file("https://www2.census.gov/geo/tiger/TIGER2021/PRIMARYROADS/tl_2021_us_primaryroads.zip", temp)
-unzip(zipfile = temp, exdir = "../raw_data/")
-roads <- read_sf("../raw_data/tl_2021_us_primaryroads.shp")
+# check if tl_2021_us_primaryroads.shp exists in "../raw_data/roads", otherwise download all required files from google drive folder
+
+if (!file.exists("../raw_data/roads/tl_2021_us_primaryroads.shp")) {
+  # create directory if it does not exist
+  dir.create("../raw_data/roads", recursive = TRUE)
+  # iterate over files and download
+  drive_ls("lasse-frank-test-dir/tl_2021_us_primaryroads") |> 
+    # filter name that end with .shp, .shx, .dbf, .prj
+    filter(str_detect(name, ".shp$|.shx$|.dbf$|.prj$")) |>
+    pull(name) |> 
+    map(~ drive_download(paste0("lasse-frank-test-dir/tl_2021_us_primaryroads/", .x), 
+                         path = paste0("../raw_data/roads/", .x), 
+                         overwrite = TRUE))
+}
 
