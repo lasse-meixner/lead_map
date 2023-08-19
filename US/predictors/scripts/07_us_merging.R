5255804c5def3503bdce267638c530e83426a8cd
Lasse Meixner <lassemeixner@gmail.com>
2023-08-19 22:47:54 +0200

    add TOdo

---
+++ US/predictors/scripts/07_us_merging.R
@@ -0,0 +1,7 @@
+# US merging 
+
+# TODO:
+
+# NOTE:
+# 1) to join on complete list of tracts for spatial predictors 
+# 2) think about aggregation to ZIPs for those states in which only ZIP level data is available (use USPS API!)
