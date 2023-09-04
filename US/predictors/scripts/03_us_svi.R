a2971b6bd1dbf0fefa071ac59a1257de032dbaf7
Lasse Meixner <lassemeixner@gmail.com>
2023-09-04 23:11:46 +0200

    rename TRACT for merging of svi

--- US/predictors/scripts/03_us_svi.R
+++ US/predictors/scripts/03_us_svi.R
@@ -10,17 +10,18 @@ svi <- drive_get("Lead_Map_Project/US/raw_data/SVI_2010_US.csv") |>
   read_csv()
 
 svi <- svi |>
+  select(-TRACT) |> 
   rename("ST" = 1,
          "svi_socioeconomic_pctile" = "R_PL_THEME1",
          "svi_disability_pctile" = "R_PL_THEME2",
          "svi_minority_lang_pctile" = "R_PL_THEME3",
          "svi_housing_transprt_pctile" = "R_PL_THEME4",
          "svi_pctile" = "R_PL_THEMES",
-         "tract" = "FIPS") |>
+         "TRACT" = "FIPS") |>
   select(c(ST:LOCATION, svi_socioeconomic_pctile, svi_disability_pctile, 
            svi_minority_lang_pctile, svi_housing_transprt_pctile, svi_pctile)) |>
   mutate(across(.cols = svi_socioeconomic_pctile:svi_pctile, .fns = ~ifelse(. == -999, NA, .)),
-         tract = as.character(tract))
+         TRACT = as.character(TRACT))
 
  # save to processed_data
 svi |> 
