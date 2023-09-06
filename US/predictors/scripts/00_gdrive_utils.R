7baeffd536435ebdce4af084f0de6a236dcf3335
Lasse Meixner <lassemeixner@gmail.com>
2023-09-06 23:29:15 +0200

    upload with time of last modification

--- US/predictors/scripts/00_gdrive_utils.R
+++ US/predictors/scripts/00_gdrive_utils.R
@@ -1,14 +1,19 @@
 library(googledrive)
 
 drive_upload_w_tstamp <- function(tbl_name) {
-    current_time  <- format(Sys.time(), "%Y-%m-%d_%H:%M:%S")
+    
+    file_path <- paste0("../processed_data/", tbl_name, ".csv")
+    
+    # get date and time of when file in ../processed_data/tbl_name.csv was last modified
+    file_mod_time <- file.info(file_path)$mtime
+
     drive_upload(
-        media = paste0("../processed_data/", tbl_name, ".csv"),
+        media = file_path,
         path = paste0(
         "Lead_Map_Project/US/processed_data/",
         tbl_name,
         "_@",
-        current_time,
+        file_mod_time,
         ".csv"),
         overwrite = TRUE)
 }
