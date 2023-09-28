e27697f9959709c11f6228cd5dc6543133788a58
Lasse Meixner <lassemeixner@gmail.com>
2023-09-28 15:55:43 +0200

    update gdrive_util files in all sections

--- US/lead_data/reuters/scripts/00_gdrive_utils.R
+++ US/lead_data/reuters/scripts/00_gdrive_utils.R
@@ -1,6 +1,26 @@
+# Some utility functions to access parked files (for which download links were no longer available) on the project GDrive directory.
+# Since these are already saved remotely and not modified, we do not version control them. CSV files are read directly into memtory, others are saved in raw_data (see ReadMe there for more information).
+
 library(googledrive)
 library(purrr)
 require(tidyverse)
+
+# function to check if file has been downloaded already (would only be used in non-csv case), and download otherwise
+gdrive_get_file <- function(file_name) {
+    # download if file_name does not already exists in "../raw_data/"
+    if (!file.exists(paste0("../raw_data/", file_name))) {
+        print(paste0("Downloading file: ", file_name))
+        drive_download(
+            file = paste0("Lead_Map_Project/US/predictors/raw_data/", file_name), 
+            path = paste0("../raw_data/", file_name), 
+            overwrite = TRUE)
+    }
+    # if not, abort
+    else {
+        print(paste0("File ", file_name, " already exists."))
+        return()
+    }
+}
 
 # function to download all files in folder:
 
@@ -12,7 +32,7 @@ gdrive_get_folder <- function(path, filetypes = ".csv$|.shp$|.shx$|.dbf$|.prj$")
     }
     # if not, abort
     else {
-        stop("Folder already exists")
+        print("Folder already exists, skipping download.")
     }
     drive_ls(path) |>
         filter(str_detect(name, filetypes)) |>
@@ -32,7 +52,7 @@ gdrive_upload_w_tstamp <- function(tbl_name) {
     drive_upload(
         media = file_path,
         path = paste0(
-        "Lead_Map_Project/US/processed_data/",
+        "Lead_Map_Project/US/predictors/processed_data/",
         tbl_name,
         "_@",
         file_mod_time,
