5f66774307e5418074ee1e9c2663e9b1cfbf97ea
Lasse Meixner <lassemeixner@gmail.com>
2024-07-15 15:26:40 +0200

    finalize data loading wrapper

--- models/model_analytics.R
+++ models/model_analytics.R
@@ -4,6 +4,18 @@ require(dplyr)
 require(ggplot2)
 library(reshape2)
 
+# requires root/init.R
+
+# import lead data
+find_and_set_directory("US/lead_data/reuters/scripts")
+source("00_merging_functions.R")
+
+# import pred data: check if tract_data and zip_data are loaded, otherwise load them
+if (!exists("tract_data") || !exists("zip_data")) {
+  find_and_set_directory("US/predictors/processed_data")
+  tract_data  <- read_csv("final_tract.csv")
+  zip_data  <- read_csv("final_zip.csv")
+}
 
 ### data preprocessing functions
 zip_info_vars <- c("zip","STATE_ABBR")
@@ -11,10 +23,11 @@ tract_info_vars <- c("TRACT","STATE_NAME","COUNTY")
 offset_var <- c("under_yo5_pplE","ped_per_100k")
 features <- c("median_annual_incomeE","house_price_medianE","poor_fam_propE","black_ppl_propE", "bp_pre_1959E_prop", "svi_socioeconomic_pctile")
 
-# HIGH LEVEL loader
-single_state_data <- function(state_name, drop_outcome = c(), info_vars = tract_info_vars, pred_preprocess_func = NULL){
+
+# HIGH LEVEL loader for tracts
+single_state_tract <- function(state_name, drop_outcome = c(), info_vars = tract_info_vars, pred_preprocess_func = NULL){
     # load and assign the state data
-    load_state(state_name, from_raw = TRUE) # from 00_merging_functions.R 
+    load_state(state_name, from_raw = FALSE) # from 00_merging_functions.R 
     state_data <- get(str_to_lower(state_name))
     # preprocess lead data
     state_lead <- state_data |>
@@ -28,6 +41,29 @@ single_state_data <- function(state_name, drop_outcome = c(), info_vars = tract_
     state_merged <- state_pred |> 
         left_join(state_lead, by = c("TRACT" = "tract")) |> 
         final_checks(drop=drop_outcome)
+}
+
+# HIGH LEVEL loader for zips
+
+single_state_zip <- function(state_name, drop_outcome = c(), info_vars = zip_info_vars, pred_preprocess_func = NULL){
+    # load and assign the state data
+    load_state(state_name, from_raw = TRUE) # from 00_merging_functions.R 
+    state_data <- get(str_to_lower(state_name))
+    # preprocess lead data
+    state_lead <- state_data |>
+        filter(year == 2010) |> #NOTE: 2010 is in the middle of our period and has max testing in many states
+        # ensure zip is chr
+        mutate(zip = as.character(zip)) |>
+        # preprocess lead data
+        preprocess_lead_data()
+      
+    state_pred <- zip_data |> filter(STATE_ABBR == state_name) |> preprocess_pred_data(info_vars = info_vars, additional_preprocess = pred_preprocess_func)
+
+    # merge
+    state_merged <- state_pred |> 
+        left_join(state_lead, by = "zip") |> 
+        final_checks(drop=drop_outcome)
+
 }
 
 preprocess_pred_data <- function(zip_or_tract_data, info_vars, additional_preprocess = NULL){
