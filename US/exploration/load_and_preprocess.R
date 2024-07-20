22117ec2975559ff284d93700452ae4b69531d4b
Lasse Meixner <lassemeixner@gmail.com>
2024-07-20 16:41:10 +0200

    make robust to also BLL_geq_10 and clean

--- US/exploration/load_and_preprocess.R
+++ US/exploration/load_and_preprocess.R
@@ -1,4 +1,4 @@
-# auxiliary functions to create basic descriptives and summary statistics
+# auxiliary functions to load and preprocess data
 
 require(dplyr)
 require(ggplot2)
@@ -29,20 +29,19 @@ single_state_tract <- function(state_name, drop_outcome = c(), pred_preprocess_f
     # load and assign the state data
     load_state(state_name, from_raw = FALSE) # from 00_merging_functions.R 
     state_data <- get(str_to_lower(state_name))
+    
+    # Apply conditional filtering based on filter_year
+    if (!is.null(filter_year)) {
+        state_data <- state_data |> filter(year == filter_year)
+    }
+    
     # preprocess lead data
-    state_lead <- state_data |>
-        # optionally filter year if provided, e.g. 2010
-        {
-            if (!is.null(filter_year)) {
-                . |> filter(year == filter_year)
-            } else {
-                .
-            }
-        } |>
-        # preprocess lead data
-        preprocess_lead_data()
-      
-    state_pred <- tract_data |> filter(STATE_ABBR == state_name) |> preprocess_pred_data(info_vars = tract_info_vars, additional_preprocess = pred_preprocess_func)
+    state_lead <- state_data |> preprocess_lead_data()
+    
+    # preprocess pred data
+    state_pred <- tract_data |> 
+      filter(STATE_ABBR == state_name) |> 
+      preprocess_pred_data(info_vars = tract_info_vars, additional_preprocess = pred_preprocess_func)
 
     # merge
     state_merged <- state_pred |> 
@@ -53,30 +52,30 @@ single_state_tract <- function(state_name, drop_outcome = c(), pred_preprocess_f
 # HIGH LEVEL loader for zips
 single_state_zip <- function(state_name, drop_outcome = c(), pred_preprocess_func = NULL, filter_year = NULL){
     # load and assign the state data
-    load_state(state_name, from_raw = TRUE) # from 00_merging_functions.R 
+    load_state(state_name, from_raw = FALSE) # from 00_merging_functions.R 
     state_data <- get(str_to_lower(state_name))
+    
+    # Apply conditional filtering based on filter_year
+    if (!is.null(filter_year)) {
+        state_data <- filter(state_data, year == filter_year)
+    }
+    
     # preprocess lead data
     state_lead <- state_data |>
-        # optionally filter year if provided, e.g. 2010
-        {
-            if (!is.null(filter_year)) {
-                . |> filter(year == filter_year)
-            } else {
-                .
-            }
-        } |>
         # ensure zip is chr
         mutate(zip = as.character(zip)) |>
         # preprocess lead data
         preprocess_lead_data()
-      
-    state_pred <- zip_data |> filter(STATE_ABBR == state_name) |> preprocess_pred_data(info_vars = zip_info_vars, additional_preprocess = pred_preprocess_func)
+    
+    # preprocess pred data
+    state_pred <- zip_data |> 
+        filter(STATE_ABBR == state_name) |> 
+        preprocess_pred_data(info_vars = zip_info_vars, additional_preprocess = pred_preprocess_func)
 
     # merge
     state_merged <- state_pred |> 
         left_join(state_lead, by = "zip") |> 
         final_checks(drop=drop_outcome)
-
 }
 
 ### auxiliary functions for high level wrapper above
@@ -108,62 +107,40 @@ preprocess_pred_data <- function(zip_or_tract_data, info_vars, additional_prepro
 
 preprocess_lead_data <- function(lead_data){
     #' preprocesses lead data
-    lead_data |> 
-        # create indicator if starts with "<" (supressed value) for both tested and BLL_geq_5
+    
+    # Dynamically check for the presence of BLL_geq_5 and BLL_geq_10
+    has_bll_geq_5 <- "BLL_geq_5" %in% names(lead_data)
+    has_bll_geq_10 <- "BLL_geq_10" %in% names(lead_data)
+    
+    # Combine all mutate operations and process BLL_geq_5 and BLL_geq_10 if present
+    lead_data <- lead_data |> 
         mutate(tested_suppressed = str_detect(tested, "<"),
-               BLL_geq_5_suppressed = str_detect(BLL_geq_5, "<")) |>
-        # remove "<" from tested and BLL_geq_5
-        mutate(tested = str_remove(tested, "<"),
-               BLL_geq_5 = str_remove(BLL_geq_5, "<")) |>
-        # convert to numeric
-        mutate(tested = as.numeric(tested),
-               BLL_geq_5 = as.numeric(BLL_geq_5)) |>
-        # add state level censoring threshold
+               tested = as.numeric(str_remove(tested, "<")),
+               BLL_geq_5_suppressed = if(has_bll_geq_5) str_detect(BLL_geq_5, "<") else NA,
+               BLL_geq_5 = if(has_bll_geq_5) as.numeric(str_remove(BLL_geq_5, "<")) else NA,
+               BLL_geq_10_suppressed = if(has_bll_geq_10) str_detect(BLL_geq_10, "<") else NA,
+               BLL_geq_10 = if(has_bll_geq_10) as.numeric(str_remove(BLL_geq_10, "<")) else NA) |>
         group_by(state) |>
-        # get minimum of BLL_geq_5 that is greater than 0
-        mutate(ell = min(BLL_geq_5[BLL_geq_5>0], na.rm = TRUE) - 1) |> #Note: I take -1 here, so "<6" will be a 5
+        mutate(ell_5 = if(has_bll_geq_5) min(BLL_geq_5[BLL_geq_5 > 0], na.rm = TRUE) - 1 else NA,
+               ell_10 = if(has_bll_geq_10) min(BLL_geq_10[BLL_geq_10 > 0], na.rm = TRUE) - 1 else NA) |>
         ungroup()
+    
+    # Remove columns that are entirely NA
+    lead_data <- lead_data[, colSums(is.na(lead_data)) < nrow(lead_data)]
+    
+    return(lead_data)
 }
 
 final_checks  <- function(merged_data, drop="BLL_geq_10"){
-    #' implements final data checks and drops unused outcome column
-    # get outcome of interest based on drop (pick the other respectively)
-    outcome_of_interest <- ifelse(drop == "BLL_geq_10", "BLL_geq_5", "BLL_geq_10")
+    #' implements final data checks and optionally drop outcome variable
     merged_data |>
         filter(under_yo5_pplE>=tested,
                tested>0) |> 
-        select(-all_of(drop)) |>
-        # drop if NA in any of offset_var, features or drop
-        drop_na(any_of(c(features, offset_var, outcome_of_interest)))
+        select(-all_of(drop))
+        # drop if NA in any of offset_var, features or outcomes
+        # get presence of outcomes by substr BLL_geq_*
+    outcome_vars <- merged_data |> names() |> str_subset("BLL_geq_") |> str_remove("_suppressed") |> unique()
+
+    merged_data |> drop_na(any_of(c(features, offset_var, outcome_vars)))
 }
 
-
-### Data plotting
-
-# Correlation plot
-get_corplot <- function(state_data, features = c("median_annual_incomeE","house_price_medianE","poor_fam_propE","black_ppl_propE", "bp_pre_1959E_prop", "svi_socioeconomic_pctile")){
-  # use geomtile to plot correlation matrix
-  state_data |> 
-    select(all_of(features)) |> 
-    cor() |> 
-    melt() |>
-    ggplot(aes(Var1, Var2, fill = value)) +
-    geom_tile() +
-    geom_text(aes(label = sprintf("%.2f", value)), color = "black", size = 3) +
-    scale_fill_gradient2(low = "blue", high = "red", mid = "white", midpoint = 0, limit = c(-1, 1)) +
-    theme_minimal() + 
-    theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-    coord_fixed()
-}
-
-# Get spread of (normalized features) in the data
-get_spread_plot <- function(state_data, features = c("median_annual_incomeE", "poor_fam_propE")){
-    # plot in grid, 3 cols
-    state_data |> 
-        select(all_of(features)) |> 
-        gather() |> 
-        ggplot(aes(value)) +
-        geom_histogram(bins = 30, alpha = 0.5) +
-        facet_wrap(~key, scales = "free") +
-        theme_minimal()
-}
