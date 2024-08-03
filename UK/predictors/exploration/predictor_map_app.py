95ba2a0163992fccce40bb88d2f14ac418154de9
Lasse Meixner <lassemeixner@gmail.com>
2024-08-03 21:30:27 +0200

    add PHE-style decile msoa-level risk map tab to dash app

--- UK/predictors/exploration/predictor_map_app.py
+++ UK/predictors/exploration/predictor_map_app.py
@@ -24,6 +24,18 @@ def get_msoa_merged_shapefile():
 
     return merged, cols
 
+# auxiliary function to compute deciles of selected predictor vars and a risk score
+def compute_risk_score(df, predictors):
+    # get copy of df
+    df_copy = df.copy()
+    # compute deciles
+    for p in predictors:
+        df_copy[p + "_decile"] = pd.qcut(df_copy[p], 10, labels=False)
+    # compute risk score
+    df_copy["risk_score"] = df_copy[[p + "_decile" for p in predictors]].sum(axis=1)
+    df_copy["risk_score"] = pd.qcut(df_copy["risk_score"], 10, labels=False) + 1 # risk score from 1-10
+    return df_copy
+
 # load merged file from england_msoa_2011.shp and combined_msoa.csv
 merged, cols = get_msoa_merged_shapefile()
 
@@ -33,33 +45,57 @@ app = dash.Dash(__name__)
 # Create app layout
 app.layout = dash.html.Div([
     dash.html.H1("UK Predictors Exploration"),
+    # add text input for searching
     dash.html.Div([
-        dash.html.Div([
-        dash.html.H3("Select Predictor"),
-        dash.dcc.Dropdown(
-            id="predictor",
-            options=[{"label": x, "value": x} for x in cols],
-            value=cols[0],
-            clearable=True,
-            searchable=True
-            ),
-        ]),
-        # add text input for searching
-        dash.html.Div([
-            dash.html.H3("Search for MSOA"),
-            dash.dcc.Input(
-                id="search",
-                type="text",
-                placeholder="Search for MSOA",
-                value="Oxford"
-            )
-
-        ]),
-        dash.html.Div([
-            # loading spinner
-            dash.dcc.Loading(
-            dash.dcc.Graph(id="choropleth")
+        dash.html.H3("Search for MSOA"),
+        dash.dcc.Input(
+            id="search",
+            type="text",
+            placeholder="Search for MSOA",
+            value="Oxford",
+            style={'fontSize': '20px'}
         )
+    ]),
+    dash.html.Br(),
+    dash.html.Div([
+        dash.dcc.Tabs([
+            dash.dcc.Tab(label = "MSOA predictor map", children=[
+                dash.html.Div([
+                dash.html.H3("Select Predictor"),
+                dash.dcc.Dropdown(
+                    id="predictor",
+                    options=[{"label": x, "value": x} for x in cols],
+                    value=cols[0],
+                    clearable=True,
+                    searchable=True
+                    ),
+                ]),
+                dash.html.Div([
+                    # loading spinner
+                    dash.dcc.Loading(
+                    dash.dcc.Graph(id="choropleth")
+                    )
+                ])
+            ]),
+            dash.dcc.Tab(label = "Decile Risk Score Map", children=[
+                dash.html.Div([
+                dash.html.H3("Select Predictors"),
+                dash.dcc.Dropdown(
+                    id="predictors",
+                    options=[{"label": x, "value": x} for x in cols],
+                    value=[cols[0]],
+                    multi=True,
+                    clearable=True,
+                    searchable=True
+                    ),
+                ]),
+                dash.html.Div([
+                    # loading spinner
+                    dash.dcc.Loading(
+                    dash.dcc.Graph(id="choropleth_risk")
+                    )
+                ])
+            ])
         ])
     ])
 ])
@@ -94,6 +130,41 @@ def update_choropleth(predictor, search):
         zoom=6,
         opacity=0.5,
         labels={predictor: predictor}
+    )
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    return fig
+
+@app.callback(
+    Output("choropleth_risk", "figure"),
+    Input("predictors", "value"),
+    Input("search", "value")
+)
+def update_choropleth_risk(predictors, search):
+    # subset data based on whether substring is in name IF search is not empty
+    if len(search) == 0:
+        merged_sub = merged
+    else:
+        merged_sub = merged[merged["msoa_name_x"].str.contains(search)]
+    # compute risk score
+    merged_sub = compute_risk_score(merged_sub, predictors)
+    # plot
+    fig = px.choropleth_mapbox(
+        merged_sub,
+        # add all predictors' deciles
+        hover_data=["msoa_name_x", "risk_score"] + [p + "_decile" for p in predictors],
+        geojson=merged_sub.geometry,
+        locations=merged_sub.index,
+        # center on England
+        center={"lat": 52.5, "lon": -1.5},
+        # size based on browser window
+        height=900,
+        color="risk_score",
+        color_continuous_scale="RdYlBu_r",
+        range_color=(merged_sub["risk_score"].min(), merged_sub["risk_score"].max()),
+        mapbox_style="carto-positron",
+        zoom=6,
+        opacity=0.5,
+        labels={"risk_score": "Risk Score"}
     )
     fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
     return fig
