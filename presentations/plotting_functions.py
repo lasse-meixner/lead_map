6caeff066f2335ef1b594e0416761f58826cbfd8
Lasse Meixner <lassemeixner@gmail.com>
2024-11-06 19:37:52 +0100

    updates to plots and structure

--- presentations/plotting_functions.py
+++ presentations/plotting_functions.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 # plot function that shows geospatial distribution of predictor across leeds
 def plot_chloropleth(variable, df, location = "Leeds"):
@@ -28,21 +29,33 @@ def plot_chloropleth(variable, df, location = "Leeds"):
     return fig
 
 # plot function that compares variable distribution selected location (e.g. leeds) with rest of UK
-def plot_histograms(variable, df, location="Leeds", show_UK=True, n_bins=16):
+def plot_histograms(variable, df, location="Leeds", other_location = "UK", n_bins=16):
     # filter for leeds
     leeds = df.loc[df["msoa_name_x"].str.contains(location)]
 
-    # Compute custom bin edges using the UK data vector
-    bin_edges = np.histogram_bin_edges(df[variable], bins=n_bins)
+    # check if variable containts "*prop"
+    if "prop" in variable:
+        bin_edges = np.linspace(0, 1, num=11)
+    else:
+        # Compute custom bin edges using the UK data vector
+        bin_edges = np.histogram_bin_edges(df[variable], bins=n_bins)
 
     # Create a Plotly figure
     fig = go.Figure()
+    hist_x_bins = dict(start=bin_edges[0], end=bin_edges[-1], size=(bin_edges[1] - bin_edges[0]))
+
 
     # Add histograms to the figure with normalization to show proportions and custom bin edges
-    if show_UK:
-        fig.add_trace(go.Histogram(x=df[variable], name="UK", histnorm='probability', xbins=dict(start=bin_edges[0], end=bin_edges[-1], size=(bin_edges[1] - bin_edges[0]))))
+    if other_location == "UK":
+        fig.add_trace(go.Histogram(x=df[variable], name="UK", histnorm='probability', xbins=hist_x_bins))
+    elif isinstance(other_location, str) and other_location != "UK":
+        other = df.loc[df["msoa_name_x"].str.contains(other_location)]
+        fig.add_trace(go.Histogram(x=other[variable], name=other_location, histnorm='probability', xbins=hist_x_bins))
+    else:
+        fig.add_trace(go.Histogram(x=leeds[variable], name=location, histnorm='probability', xbins=hist_x_bins))
 
-    fig.add_trace(go.Histogram(x=leeds[variable], name=location, histnorm='probability', xbins=dict(start=bin_edges[0], end=bin_edges[-1], size=(bin_edges[1] - bin_edges[0]))))
+    # overlay leeds
+    fig.add_trace(go.Histogram(x=leeds[variable], name=location, histnorm='probability', xbins=hist_x_bins))
 
     # Customize the layout
     fig.update_layout(
@@ -50,12 +63,81 @@ def plot_histograms(variable, df, location="Leeds", show_UK=True, n_bins=16):
         xaxis_title=variable,
         yaxis_title="Proportion",
         barmode='overlay'
+        # todo: add tick markers
     )
 
     # Set the opacity for better visualization
     fig.update_traces(opacity=0.75)
 
     return fig
+
+def plot_pairs(variables, df, location="Leeds"):
+    # filter for leeds
+    leeds = df.loc[df["msoa_name_x"].str.contains(location)]
+    
+    # create pairsplot
+    n = len(variables)
+    
+    fig = make_subplots(
+        rows=n, cols=n,
+        shared_xaxes=False,
+        shared_yaxes=False,
+        vertical_spacing=0.1,
+        horizontal_spacing=0.1
+    )
+    
+    for i in range(n):
+        for j in range(n):
+            if i == j:
+                # Diagonal: Density plot
+                fig.add_trace(
+                    go.Histogram(x=leeds[variables[i]], nbinsx=20, histnorm='probability'),
+                    row=i+1, col=j+1
+                )
+            elif i > j:
+                # Lower diagonal: Scatter plot
+                fig.add_trace(
+                    go.Scatter(x=leeds[variables[j]], y=leeds[variables[i]], mode='markers'),
+                    row=i+1, col=j+1
+                )
+                
+                # Calculate regression line
+                x = leeds[variables[j]]
+                y = leeds[variables[i]]
+                m, b = np.polyfit(x, y, 1)
+                fig.add_trace(
+                    go.Scatter(x=x, y=m*x + b, mode='lines', line=dict(color='red')),
+                    row=i+1, col=j+1
+                )
+            else:
+                # Upper diagonal: Correlation coefficient
+                corr_value = leeds[variables[i]].corr(leeds[variables[j]])
+                fig.add_trace(
+                    go.Scatter(
+                        x=[0.5], y=[0.5],
+                        text=[f'Corr:{corr_value:.2f}'],
+                        mode='text',
+                        textfont=dict(
+                            size=20  # Increase the font size
+                            )
+                    ),
+                    row=i+1, col=j+1
+                )
+
+            # Update axes
+            if i == n-1:
+                fig.update_xaxes(title_text=variables[j], row=i+1, col=j+1)
+            if j == 0:
+                fig.update_yaxes(title_text=variables[i], row=i+1, col=j+1)
+
+    fig.update_layout(
+        showlegend=False,
+        plot_bgcolor='white',
+        hovermode="closest"
+    )
+    
+    return fig
+
 
 # plot function that compares empirical CDF of variable in selected location (e.g. leeds) with rest of UK
 def plot_CDFs(variable, df, location="Leeds"):
